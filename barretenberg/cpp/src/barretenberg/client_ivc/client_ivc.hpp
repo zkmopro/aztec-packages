cb0cd2913ae8eaee20fae76d4f5ccd5b490eabff
moven0831 <moven0831@gmail.com>
2025-08-12 01:09:59 +0800

    fix: add constructor for StdlibVerifierInputs to enhance emplace_back compatibility

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
@@ -150,6 +150,17 @@ class ClientIVC {
         std::shared_ptr<RecursiveVKAndHash> honk_vk_and_hash;
         QUEUE_TYPE type;
         bool is_kernel = false;
+
+        // Constructor for emplace_back compatibility
+        StdlibVerifierInputs(StdlibProof proof_,
+                            std::shared_ptr<RecursiveVKAndHash> honk_vk_and_hash_,
+                            QUEUE_TYPE type_,
+                            bool is_kernel_)
+            : proof(std::move(proof_))
+            , honk_vk_and_hash(std::move(honk_vk_and_hash_))
+            , type(type_)
+            , is_kernel(is_kernel_)
+        {}
     };
     using StdlibVerificationQueue = std::deque<StdlibVerifierInputs>;
 
